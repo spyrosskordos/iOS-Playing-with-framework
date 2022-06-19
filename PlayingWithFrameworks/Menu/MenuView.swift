e3a17b277fdd5fc7a798d7c554bf92b9a7e745b7
Spyrosskordos <spyrosskordos@gmail.com>
2022-06-19 14:34:23 +0300

    added menu view

---
+++ PlayingWithFrameworks/Menu/MenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuView.swift
+//  PlayingWithFrameworks
+//
+//  Created by Sko on 19/6/22.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
