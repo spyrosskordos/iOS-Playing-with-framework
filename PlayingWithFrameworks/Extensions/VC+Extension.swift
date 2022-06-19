5b4cc4ae27645bf4675bc750b6919a722ba7143c
Spyrosskordos <spyrosskordos@gmail.com>
2022-06-19 14:31:40 +0300

    added vc for swift ui

---
+++ PlayingWithFrameworks/Extensions/VC+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  VC+Extension.swift
+//  PlayingWithFrameworks
+//
+//  Created by Sko on 19/6/22.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+extension UIViewController {
+    func addSubSwiftUIView<Content>(_ swiftUIView: Content, to view: UIView) where Content: View {
+
+        let hostingController = UIHostingController(rootView: swiftUIView)
+
+        addChild(hostingController)
+
+        view.addSubview(hostingController.view)
+
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        let constraints = [
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.leftAnchor.constraint(equalTo: view.leftAnchor),
+            view.bottomAnchor.constraint(equalTo: hostingController.view.bottomAnchor),
+            view.rightAnchor.constraint(equalTo: hostingController.view.rightAnchor)
+        ]
+
+        NSLayoutConstraint.activate(constraints)
+
+        hostingController.didMove(toParent: self)
+    }
+}
