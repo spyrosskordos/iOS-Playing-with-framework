69e43274ad735472206300d86e9488717b8aedd6
Spyrosskordos <spyrosskordos@gmail.com>
2022-06-19 17:00:10 +0300

    Added button style

---
+++ PlayingWithFrameworks/PWFStyles/PWFRadiusButtonStyle.swift
@@ -0,0 +1,21 @@
+//
+//  PWFRadiusButtonStyle.swift
+//  PlayingWithFrameworks
+//
+//  Created by Sko on 19/6/22.
+//
+
+import SwiftUI
+
+struct PWFRadiusButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color(red: 0, green: 0, blue: 0.5))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+
