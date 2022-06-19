6bf82b1ff8e05aaeb6405ad2dcb7635f63cb4a04
Spyrosskordos <spyrosskordos@gmail.com>
2022-06-19 14:30:26 +0300

    vc files

--- PlayingWithFrameworks/List/ListViewController.swift
+++ PlayingWithFrameworks/List/ListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ListViewController.swift
 //  PlayingWithFrameworks
 //
 //  Created by Sko on 19/6/22.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
---
+++ PlayingWithFrameworks/Menu/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayingWithFrameworks
+//
+//  Created by Sko on 19/6/22.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
