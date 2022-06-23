a85b13fb588f6273a2c12f4076074ce56605756f
Spyrosskordos <spyrosskordos@gmail.com>
2022-06-23 22:49:30 +0300

    createMenu package

---
+++ Menu/Tests/MenuTests/MenuTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Menu
+
+final class MenuTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Menu().text, "Hello, World!")
+    }
+}
